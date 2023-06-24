42646319568e23c74343e9d1fa8b2f6b36f56ec1
Scrub <scott-donaldson@outlook.com>
2023-06-24 22:40:06 +0100

    Updated approved authors list

--- src/util/queerCoded/lookupTables.ts
+++ src/util/queerCoded/lookupTables.ts
@@ -1,5 +1,10 @@
 const approvedAuthorsList = [
-        "did:plc:kcgqbkp3uuk3eesuzvf4s6u6"
+        "did:plc:kcgqbkp3uuk3eesuzvf4s6u6", // kaitlyn.dev
+        "did:plc:ij5756nvubeeqo2kn6eeezdx", // astrid.catgirl.family
+        "did:plc:qruaxn2eewkli7hsvzxo5hwf", // eva.computer
+        "did:plc:l7buhragfpktqopl7wdojhi3", // trans.bsky.social
+        "",
+        "",
     ]
 
 
